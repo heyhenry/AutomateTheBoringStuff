e85dbac1cd9ca6ecc8def06e89c61ad623842cbb
Henry Han <henryyy3@hotmail.com>
2019-10-23 07:57:23 +0800

    Revising function and each associated aspect.

--- checkPhoneNumber.py
+++ checkPhoneNumber.py
@@ -32,4 +32,45 @@ for i in range(len(message)):
 print('Done')
 
 
+def checkMyNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+print('')
+print('Secondary testing to proceed below: ')
+print('Phone number in checking: 123-456-7891')
+print(checkMyNumber('123-456-7891'))
+print('Phone number in checking: 111-222-33333')
+print(checkMyNumber('111-222-33333'))
+print('')
+print('Checking incoming voicemail: ')
+print('Good evening, please contact me through my personal number at 555-666-7891.\n'
+      'An alternative is through my workplace at 234-567-8901. Thank you.')
+print('')
+message = ('Good evening, please contact me through my personal number at 555-666-7891.'
+           'An alternative is through my workplace at 234-567-8901. Thank you.')
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if checkMyNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('')
+print('Processing . . . . . .')
+print('All check completed . . . . . ')
+
+
+
 
