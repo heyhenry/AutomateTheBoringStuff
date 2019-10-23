fdfe10ffe990d3a9735fbd73139f4fd222aa644a
Henry Han <henryyy3@hotmail.com>
2019-10-23 10:45:05 +0800

    More examples of regex uses using pipes, parenthesis and optional instances.

--- phoneNumRegex.py
+++ phoneNumRegex.py
@@ -49,12 +49,53 @@ import re
 #############################################################
 
 # Fifth example of regex use:
-batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
-mo = batRegex.search('Batmobile lost a wheel')
-print(mo.group())
-print(mo.group(1))
+# batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+# mo = batRegex.search('Batmobile lost a wheel.')
+# print(mo.group())
+# print(mo.group(1))
 
+#############################################################
 
+# Sixth example of regex use:
+# batRegex = re.compile(r'Bat(wo)?man')
+# mo1 = batRegex.search('The Adventures of Batman')
+# print(mo1.group())
+#
+# mo2 = batRegex.search('The Adventures of Batwoman')
+# print(mo2.group())
 
+#############################################################
 
+# Seventh example of regex use:
+# phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+# mo1 = phoneRegex.search('My number is 415-555-4242.')
+# print(mo1.group())
+#
+# mo2 = phoneRegex.search('My number is 555-4242.')
+# print(mo2.group())
 
+#############################################################
+
+# Eighth example of regex use:
+# batRegex = re.compile(r'Bat(wo)*man')
+# mo1 = batRegex.search('The Adventures of Batman')
+# print(mo1.group())
+#
+# mo2 = batRegex.search('The Adventures of Batwoman')
+# print(mo2.group())
+#
+# mo3 = batRegex.search('The Adventures of Batwowowowoman')
+# print(mo3.group())
+
+#############################################################
+
+# Ninth example of regex use:
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+print(mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwowowoman')
+print(mo2.group())
+
+mo3 = batRegex.search('The Adventures of Batman')
+print(mo3 == None)
