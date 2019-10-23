66c0a754fd81583235c9015df923b58e34156558
Henry Han <henryyy3@hotmail.com>
2019-10-23 12:13:28 +0800

    More examples of regex uses using pipes, parenthesis and optional instances.

--- phoneNumRegex.py
+++ phoneNumRegex.py
@@ -90,12 +90,33 @@ import re
 #############################################################
 
 # Ninth example of regex use:
-batRegex = re.compile(r'Bat(wo)+man')
-mo1 = batRegex.search('The Adventures of Batwoman')
+# batRegex = re.compile(r'Bat(wo)+man')
+# mo1 = batRegex.search('The Adventures of Batwoman')
+# print(mo1.group())
+#
+# mo2 = batRegex.search('The Adventures of Batwowowoman')
+# print(mo2.group())
+#
+# mo3 = batRegex.search('The Adventures of Batman')
+# print(mo3 == None)
+
+#############################################################
+
+# Tenth example of regex use:
+# haRegex = re.compile(r'(Ha){3}')
+# mo1 = haRegex.search('HaHaHa')
+# print(mo1.group())
+#
+# mo2 = haRegex.search('Ha')
+# print(mo2 == None)
+
+#############################################################
+
+# Eleventh example of regex use:
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
 print(mo1.group())
 
-mo2 = batRegex.search('The Adventures of Batwowowoman')
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
 print(mo2.group())
-
-mo3 = batRegex.search('The Adventures of Batman')
-print(mo3 == None)
